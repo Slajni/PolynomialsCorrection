5426491f48fcaa1a924d99e7c7001e5d3a429c9f
Damian <c.damiankucharski@gmail.com>
2018-09-23 20:19:25 +0200

    Damian

--- main.cpp
+++ main.cpp
@@ -11,24 +11,19 @@ public:
     //default constructor
     Polynomials(int deg)
     {
-        degree = 0;
-        coefficients = new int[deg];
-        for (int i = 0; i < 10; i++)
+        degree = deg;
+        coefficients = new int[deg+1];
+        for (int i = 0; i <= deg; i++)
             coefficients[i] = 0;
     }
 
-    void SetCoeffs(int a, int b)
-    {
-        coefficients[a]=b;
-    }
-
     //printing
-    void print(Polynomials& p)
+    void print()
     {
-        cout << coefficients << "x^" << degree;
-        for (int i = degree - 1; i>=0; i--)
+        cout << coefficients[degree] << "x^" << degree << " + ";
+        for (int i = degree-1; i >= 0; i--)
             cout << " + " << coefficients[i] << "x^" << i;
-        return;
+        cout << endl;
     }
 
     ~Polynomials() { delete coefficients; };
@@ -43,58 +38,58 @@ public:
     }
 
     //addition
-    Polynomials operator+(const Polynomials& p2)
+    Polynomials* operator+(const Polynomials& p2)
     {
         int degree;
         if (this->degree >= p2.degree)
-            degree = this->degree;
-        else
             degree = p2.degree;
+        else
+            degree = this->degree;
 
-        Polynomials p(degree);
-        p.coefficients = new int[p.degree+1];
+        Polynomials *p = new Polynomials(degree);
+        p->coefficients = new int[p->degree+1];
 
-        for (int i = p.degree; i >=0; i--)
-            p.coefficients[i] = this->coefficients[i]+p2.coefficients[i];
+        for (int i = p->degree; i >= 0; i--)
+            p->coefficients[i] = this->coefficients[i]+p2.coefficients[i];
         return p;
     }
 
     //multiplication
-    Polynomials operator*(const Polynomials& p2)
+    Polynomials* operator*(const Polynomials& p2)
     {
-        Polynomials p(this->degree + p2.degree);
+        Polynomials * p = new Polynomials(this->degree + p2.degree);
         for ( int i = 0; i <= this->degree; i++)
             for ( int j = 0; j <= p2.degree; j++)
-                p.coefficients[i+j] += this->coefficients[i] * p2.coefficients[j];
+                p->coefficients[i+j] += this->coefficients[i] * p2.coefficients[j];
         return p;
     }
 
     //evaluation for given argument
-    int Evaluate(Polynomials& p)
+    int Evaluate()
     {
         int x;
         int sum;
         cout << "\nEnter the value of x: ";
         cin >> x;
         for (int i =0; i < degree; i++)
-            int sum = sum + p.coefficients[i]*pow(x,degree-i);
+            int sum = sum + this->coefficients[i]*pow(x,degree-i);
         return sum;
+    }
+
+    void setCoefficients()
+    {
+        int coefficient;
+        cout << "Enter coefficients of your polynomial: " << endl;
+        for (int i = 0; i <= degree; i++)
+        {
+            cout << "x^" << i << ": ";
+            cin >> coefficient;
+            coefficients[i] = coefficient;
+        }
     }
 
 };
 
-void First(Polynomials *p1, int degree)
-{
-    int coefficient;
-    cout << "Enter coefficients of your polynomial: " << endl;
-    for (int i = 0; i <= degree; i++)
-    {
-        cout << "x^" << i << ": ";
-        cin >> coefficient;
-        if(coefficient != 0)
-            p1->SetCoeffs(coefficient,i);
-    }
-}
 
 void Printing( Polynomials *p1, Polynomials *p2)
 {
@@ -107,21 +102,26 @@ void Printing( Polynomials *p1, Polynomials *p2)
 
 int main()
 {
-    int d1,d2;
+    int d1 = 0;
+    int d2 = 0;
 
     cout << "First polynomial\n" << "What is the degree of your first polynomial? ";
     cin >> d1;
     Polynomials p1(d1);
-    First(&p1, d1);
-
-    cout << "Second polynomial\n" << "What is the degree of your second polynomial? ";
+    p1.print();
+    p1.setCoefficients();
+    p1.print();
+    cout << "\nSecond polynomial\n" << "What is the degree of your second polynomial? ";
     cin >> d2;
     Polynomials p2(d2);
-    First(&p2, d2);
-
+    p2.setCoefficients();
+    p2.print();
     /*Polynomials result((d1 > d2) ? d1 : d2);
     Printing(&p1, &p2);
     Result(&p1, &p2, &result); */
+    Polynomials * result = p1 * p2;
+    result->print();
+
 
     return 0;
 }
